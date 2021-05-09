fa7d56f7d5c0f8c4db0b810526ce97caf7697a7e
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-05-09 14:38:01 +0300

    Create higher_lower_game.py

---
+++ Higher Lower game/higher_lower_game.py
@@ -0,0 +1,13 @@
+#Generate the Header of the game.
+  #from art import logo.
+
+#from game_data import data.
+#Find the way to retrieve information from the dictionary list.
+#The information that you are going to retrieve is:
+  #name, -> Shown in the screen.
+  #description, -> Show in the screen.
+  #country -> Show in the screen.
+
+  #follower_count -> User for calculation of the result.
+
+#Create two variables A and B.
